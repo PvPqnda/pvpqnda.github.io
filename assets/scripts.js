a872a856c3416854e89ec2dc0ce376c5953c7023
Hielke <hielkemaps@gmail.com>
2023-05-18 08:58:11 +0200

    Revamped navbar + Search field

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,17 @@
 $(function() {
+
+	// Search bar
+	$('#searchInput').on('input', function () {
+        var filter = $(this).val().toLowerCase();
+        $('.card-deck .card').each(function () {
+            if ($(this).find('.card-title').text().toLowerCase().indexOf(filter) > -1) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+
 	//Remove commento text
     const observer = new MutationObserver(function(mutationsList) {
         for (let mutation of mutationsList) {
@@ -27,6 +40,50 @@ $(document).on('hidden.bs.modal', function () {
 		$('body').addClass('modal-open');
 	}
 });
+
+function toggleMobileMenu() {
+    var $toggleBtn = $('[data-toggle="toggle-menu"]');
+    var $navMenuCont = $($toggleBtn.data('target'));
+    var isOpen = $toggleBtn.data('isopen');
+    var isAnimating = $navMenuCont.is(':animated');
+
+    if (isAnimating) {
+        return;
+    }
+
+    if (isOpen) {
+        closeMobileMenu();
+        $toggleBtn.data('isopen', false);
+    } else {
+        openMobileMenu();
+        $toggleBtn.data('isopen', true);
+    }
+
+    function openMobileMenu() {
+        $('body').addClass('navbar-open');
+        $('.hamburger').addClass('active');
+
+        $navMenuCont.css('display', 'block');
+
+        let $navItems = $navMenuCont.find('.nav-item');
+        $navItems.removeClass('animate__fadeOutUp').addClass('animate__fadeInDown');
+        $navMenuCont.animate({ 'height': '100vh' ,'opacity': '1'}, 350);
+    }
+
+    function closeMobileMenu() {
+        $('body').removeClass('navbar-open');
+        $('.hamburger').removeClass('active');
+
+        let $navItems = $navMenuCont.find('.nav-item');
+        $navItems.removeClass('animate__fadeInDown').addClass('animate__fadeOutUp');
+		
+        $navMenuCont.removeClass('slide-in').delay(100).animate({ 'height': '0vh','opacity': '0' }, 350, function () {
+            $navMenuCont.css('display', 'none');
+        });
+    }
+}
+
+$('[data-toggle="toggle-menu"]').on('click', toggleMobileMenu);
 
 
 Fancybox.bind("[data-fancybox]", {
